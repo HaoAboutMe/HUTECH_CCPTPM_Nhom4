56365b0f83c1884c004f3186978b3585120d5b4c
Nguyen Le Hoan Hao <nguyenlehoanhao2004@gmail.com>
2026-03-30 14:05:23 +0700

    first commit

---
+++ src/main/java/com/example/webbanmaytinh/WebbanmaytinhApplication.java
@@ -0,0 +1,13 @@
+package com.example.webbanmaytinh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbanmaytinhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbanmaytinhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webbanmaytinh/WebbanmaytinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webbanmaytinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbanmaytinhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
