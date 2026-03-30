482eb52014a2a748faa6c0465ac65df9754a5de0
Nguyen Le Hoan Hao <nguyenlehoanhao2004@gmail.com>
2026-03-30 15:46:47 +0700

    feat: add entity Product and Category

---
+++ src/main/java/com/example/webbanmaytinh/entity/Category.java
@@ -0,0 +1,33 @@
+package com.example.webbanmaytinh.entity;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Product> products = new ArrayList<>();
+}
